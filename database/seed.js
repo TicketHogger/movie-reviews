fb23c1fe5c7655fdd83bf61820751a8da1ce1216
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-24 17:24:06 -0700

    Fixed variable names in seed.js

--- database/seed.js
+++ database/seed.js
@@ -2,21 +2,21 @@ const faker = require('faker');
 const fs = require('fs');
 
 const db = 'movies';
-const sqlFile = 'schema.sql';
+const sqlFileUrl = 'schema.sql';
 const rows = 10;
 
 for (let i = 0; i < rows; i++) {
   let titleLength = Math.floor(Math.random() * 5) + 1; // 1-5
 
   let username = '\"' + faker.internet.userName().replace('.', ' ') + '\"';
-  let title = '\"' + faker.lorem.sentence(titleLength).replace('.', '') + '\"';
-  let mooz = Math.floor(Math.random() * 5) + 1;
-  let review = '\"' + faker.lorem.paragraph() + '\"';
-  let helpful = Math.floor(Math.random() * 300);
+  let title = '\"' + faker.lorem.sentence(titleLength).replace('.', '') + '\"'; // title of review
+  let moozRating = Math.floor(Math.random() * 5) + 1; // rating system
+  let reviewText = '\"' + faker.lorem.paragraph() + '\"';
+  let helpfulReviews = Math.floor(Math.random() * 300);
 
-  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${mooz}, ${review}, ${helpful})\n`;
+  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${moozRating}, ${reviewText}, ${helpfulReviews})\n`;
 
-  fs.appendFile(sqlFile, sqlCommand, err => {
+  fs.appendFile(sqlFileUrl, sqlCommand, err => {
     if (err) {
       console.log('Sql file write failure', err);
     } else {
