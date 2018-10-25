b2d4ff12e9637683a4e334527da2ddda091e3d46
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-24 17:19:03 -0700

    Fixed insert command

--- database/seed.js
+++ database/seed.js
@@ -3,24 +3,24 @@ const fs = require('fs');
 
 const db = 'movies';
 const sqlFile = 'schema.sql';
+const rows = 10;
 
+for (let i = 0; i < rows; i++) {
+  let titleLength = Math.floor(Math.random() * 5) + 1; // 1-5
 
-let titleLength = Math.floor(Math.random() * 5) + 1; // 1-5
+  let username = '\"' + faker.internet.userName().replace('.', ' ') + '\"';
+  let title = '\"' + faker.lorem.sentence(titleLength).replace('.', '') + '\"';
+  let mooz = Math.floor(Math.random() * 5) + 1;
+  let review = '\"' + faker.lorem.paragraph() + '\"';
+  let helpful = Math.floor(Math.random() * 300);
 
+  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${mooz}, ${review}, ${helpful})\n`;
 
-let username = '\"' + faker.internet.userName().replace('.', ' ') + '\"';
-let title = '\"' + faker.lorem.sentence(titleLength).replace('.', '') + '\"';
-let mooz = Math.floor(Math.random() * 5) + 1;
-let review = '\"' + faker.lorem.paragraph() + '\"';
-let helpful = Math.floor(Math.random() * 300);
-
-let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) values (${username}, ${title}, ${mooz}, ${review}, ${helpful})\n`;
-
-
-fs.appendFile(sqlFile, sqlCommand, err => {
-  if (err) {
-    console.log('Sql file write failure', err);
-  } else {
-    console.log('Successful write to .sql');
-  }
-});
+  fs.appendFile(sqlFile, sqlCommand, err => {
+    if (err) {
+      console.log('Sql file write failure', err);
+    } else {
+      // console.log('Successful write to .sql');
+    }
+  });
+}
