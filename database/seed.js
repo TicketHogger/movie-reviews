c598136416358ba142f30cf6e5dca39868b43417
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-24 18:54:57 -0700

    Fixed semicolon in mysql script

--- database/seed.js
+++ database/seed.js
@@ -14,7 +14,7 @@ for (let i = 0; i < rows; i++) {
   let reviewText = '\"' + faker.lorem.paragraph() + '\"';
   let helpfulReviews = Math.floor(Math.random() * 300);
 
-  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${moozRating}, ${reviewText}, ${helpfulReviews})\n`;
+  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${moozRating}, ${reviewText}, ${helpfulReviews});\n`;
 
   fs.appendFile(sqlFileUrl, sqlCommand, err => {
     if (err) {
@@ -24,3 +24,20 @@ for (let i = 0; i < rows; i++) {
     }
   });
 }
+
+/*
+CREATE DATABASE IF NOT EXISTS fandango;
+use fandango;
+
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE movies (
+  id INT NOT NULL AUTO_INCREMENT,
+  Username VARCHAR(50) NOT NULL,
+  Title VARCHAR(50),
+  Mooz INT NOT NULL,
+  Review VARCHAR(500),
+  Helpful INT,
+  PRIMARY KEY (id)
+);
+*/
