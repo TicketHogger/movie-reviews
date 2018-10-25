4af354f9c6428a3aab8538fcefc5509cfaf00f84
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-25 13:48:16 -0700

    Basic server working

--- server/index.js
+++ server/index.js
@@ -11,8 +11,15 @@ const port = 3013;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(request, response, next) {
+  response.set({
+    'Access-Control-Allow-Origin': '*'
+  });
+  next();
+});
 
-app.get('/api/movies/:movie/reviews', (req, res) => {
+
+app.get('/api/movies/:movieid/reviews', (req, res) => {
 	res.send('hello');
 });
 
