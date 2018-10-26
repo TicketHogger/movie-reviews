529271e98fa0a49285dee0f09cf22e211bb1e4d9
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-25 19:57:36 -0700

    Changed sql seeding to csv

--- database/seed.js
+++ database/seed.js
@@ -2,62 +2,33 @@ const faker = require('faker');
 const fs = require('fs');
 
 const db = 'movies';
-const sqlFileUrl = 'schema.sql';
-const rows = 100;
+const fileUrl = 'data.csv';
+const rows = 5;
 
-const createDB = 'CREATE DATABASE IF NOT EXISTS fandango;\n\
-use fandango;\n\
-\n\
-DROP TABLE IF EXISTS movies;\n\
-\n\
-CREATE TABLE movies (\n\
-  id INT NOT NULL AUTO_INCREMENT,\n\
-  Username VARCHAR(50) NOT NULL,\n\
-  Title VARCHAR(50),\n\
-  Mooz INT NOT NULL,\n\
-  Review VARCHAR(500),\n\
-  Helpful INT,\n\
-  PRIMARY KEY (id)\n\
-);\n'
-fs.writeFile(sqlFileUrl, createDB, err => {
+const columns = 'username, title, mooz, review, helpful';
+
+fs.writeFile(fileUrl, columns, err => {
   if (err) {
-    console.log('Error creating file', err);
+    console.log('Data file title write failture', err);
   }
 });
 
 for (let i = 0; i < rows; i++) {
   let titleLength = Math.floor(Math.random() * 5) + 1; // 1-5
 
-  let username = '\"' + faker.internet.userName().replace('.', ' ') + '\"';
-  let title = '\"' + faker.lorem.sentence(titleLength).replace('.', '') + '\"'; // title of review
+  let id = i;
+  let username = '"' + faker.internet.userName().replace('.', ' ') + '"';
+  let title = '"' + faker.lorem.sentence(titleLength).replace('.', '') + '"'; // title of review
   let moozRating = Math.floor(Math.random() * 5) + 1; // rating system
-  let reviewText = '\"' + faker.lorem.paragraph() + '\"';
+  let reviewText = '"' + faker.lorem.paragraph() + '"';
   let helpfulReviews = Math.floor(Math.random() * 300);
 
-  let sqlCommand = `INSERT INTO ${db} (Username, Title, Mooz, Review, Helpful) VALUES (${username}, ${title}, ${moozRating}, ${reviewText}, ${helpfulReviews});\n`;
-
-  fs.appendFile(sqlFileUrl, sqlCommand, err => {
+  let data = '\n' + id + ',' + username + ',' + title + ',' + moozRating + ',' +  reviewText + ',' +  helpfulReviews;
+  fs.appendFile(fileUrl, data, err => {
     if (err) {
-      console.log('Sql file write failure', err);
+      console.log('Data file write failure', err);
     } else {
-      // console.log('Successful write to .sql');
+      // console.log('Successful write to .csv');
     }
   });
 }
-
-/*
-CREATE DATABASE IF NOT EXISTS fandango;
-use fandango;
-
-DROP TABLE IF EXISTS movies;
-
-CREATE TABLE movies (
-  id INT NOT NULL AUTO_INCREMENT,
-  Username VARCHAR(50) NOT NULL,
-  Title VARCHAR(50),
-  Mooz INT NOT NULL,
-  Review VARCHAR(500),
-  Helpful INT,
-  PRIMARY KEY (id)
-);
-*/
