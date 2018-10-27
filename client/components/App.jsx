f62eff72a74c6d09a64081311f88d6bffb4a860e
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-26 20:02:29 -0700

    Fixing Webpack with React

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <div>
+      cool
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'review.bundle.js'
   },
   module: {
     rules: [
-      {test: /\.jsx$/, use: 'babel-loader'}
+      { test: /\.jsx$/, use: 'babel-loader' }
     ]
   }
 };
