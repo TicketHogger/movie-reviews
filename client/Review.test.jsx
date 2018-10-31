f52a305ff17a437fa35b287d4eeb9adcc6076b32
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-10-30 17:32:02 -0700

    nothing works still

--- client/Review.test.jsx
+++ client/Review.test.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
 import {
-  configure, shallow, mount, render
+  shallow, mount, render
 } from 'enzyme';
 
 import App from './components/App';
 
-configure({ adapter: new Adapter() });
+describe('A test', () => {
+  it('should show a test', () => {
+    expect(shallow(<App />).contains(
+      <b>
+        FAN REVIEWS
+      </b>
+    ).toBe(true));
+  });
+});
 
-// describe('A test', () => {
-//   it('should show a test', () => {
-//     expect(shallow(<App />).contains(
-//       <b>
-//         FAN REVIEWS
-//       </b>
-//     ).toBe(true));
-//   });
+// test('Some sketchy test', () => {
+//   const wrapper = shallow(<App />);
+//   expect(wrapper).toContainMatchingElements(1, '.App');
 // });
