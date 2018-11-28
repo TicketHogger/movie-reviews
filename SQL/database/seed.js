aa0a6e97f13bc5e99757aec7cd5ef31054b39cfc
Jawad Muhammadi <muhammadijawad10@gmail.com>
2018-11-28 10:33:03 -0800

    testing on ec2

--- SQL/database/seed.js
+++ SQL/database/seed.js
@@ -1,8 +1,8 @@
 const faker = require('faker');
 
 const fs = require('fs');
-const file = fs.createWriteStream('./review-data.csv', { encoding: 'utf8', flags: 'a' });
-const file2 = fs.createWriteStream('./movie-data.csv', { encoding: 'utf8', flags: 'a' });
+const file = fs.createWriteStream('./review-data.csv1', { encoding: 'utf8', flags: 'a' });
+const file2 = fs.createWriteStream('./movie-data.csv1', { encoding: 'utf8', flags: 'a' });
 
 
 
@@ -17,7 +17,7 @@ file2.on('drain', function() {
 const seeder = () => {
   const start = new Date();
  
-  const rows = 5000000;
+  const rows = 5000;
 
   for (let i = 1; i < rows/1000 + 1; i += 1) {
     const titleLength = Math.floor(Math.random() * 3) + 1;
@@ -39,7 +39,7 @@ const seeder = () => {
     const Mooz = Math.floor(Math.random() * 5) + 1; // rating system
     const Review = `${faker.lorem.paragraph()}`;
     const Helpful = Math.floor(Math.random() * 300);
-    const Movie = Math.floor(Math.random() * 1000) + 1;
+    const Movie = Math.floor(Math.random() * 100) + 1;
     //const dateWritten = faker.date.past();
     const data = `\n${id},${Username},${Title},${Mooz},${Review},${Helpful},${Movie}`;
     file.write(data);
